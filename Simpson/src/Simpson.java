340062232ce7f35a4ab1d4c818453beab72cebdc
Jrvillacreses <jrvillacreses@gmail.com>
2024-10-04 12:10:20 +0200

    Add files via upload

---
+++ Simpson/src/Simpson.java
@@ -0,0 +1,21 @@
+
+public class Simpson {
+    //VISIBLE A NIVEL DE LA CLASE SIMPSON
+    char[][] tablero = new char[10][10];
+    //FIN
+
+    //Software de gestión de venta ONLINE
+    public static void rellenarTablero(char pers) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        // int contador = 0;
+        char personaje = '#'
+        rellenarTablero(personaje);
+        // contador = 10;
+        rellenarTablero(personaje);
+
+    }
+}
